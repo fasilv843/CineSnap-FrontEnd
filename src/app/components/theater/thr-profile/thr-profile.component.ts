8e11b3a6e5b69424adb021073e4c27f1b10bb2aa
Fasil <fasilvaliyattil843@gmail.com>
2023-11-24 17:14:38 +0530

    theater profile design complete

--- src/app/components/theater/thr-profile/thr-profile.component.ts
+++ src/app/components/theater/thr-profile/thr-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core'
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-thr-profile',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./thr-profile.component.css']
 })
 export class ThrProfileComponent {
+  // userDetails$ = this.store.pipe(select(selectUserDetails))
+  name: string = ''
+  email: string = ''
+  dob!: Date
+  mobile!: number
+  profilePic!: string
+  city!: string
+  state!: string
+  district!: string
+  country!: string
+  zip!: number
+  wallet!: number
+  editMode: boolean = false
 
+  constructor (
+    @Inject(Store) private readonly store: Store
+  ) {}
+
+  ngOnInit (): void {
+    // this.userDetails$.subscribe((user) => {
+    //   this.name = user?.name ?? '';
+    //   this.email = user?.email ?? '';
+    //   this.dob = user?.dob ?? new Date('1990-01-01');
+    //   this.mobile = user?.mobile ?? 0
+    //   this.profilePic = user?.profilePic ?? ''
+    //   this.city = user?.address?.city ?? ''
+    //   this.state = user?.address?.state ?? ''
+    //   this.district = user?.address?.district ?? ''
+    //   this.country = user?.address?.country ?? ''
+    //   this.zip = user?.address?.zip ?? 0
+    //   this.wallet = user?.wallet ?? 0
+    // })
+  }
+
+  toggleEditMode (): void {
+    this.editMode = !this.editMode
+  }
 }
