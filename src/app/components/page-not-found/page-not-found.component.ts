fa28ba7b4dd803dbb912afc22b5bf9ce5a8f05cf
Fasil <fasilvaliyattil843@gmail.com>
2023-11-27 12:20:11 +0530

    Page Not Found page updated

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent {
-
+  goBack (): void {
+    window.history.back()
+  }
 }
