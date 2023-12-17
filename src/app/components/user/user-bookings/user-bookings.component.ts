420f71e86f6570233bede6eb69afb220fae5fa6b
Fasil <fasilvaliyattil843@gmail.com>
2023-12-17 17:04:03 +0530

    booking design finished

--- src/app/components/user/user-bookings/user-bookings.component.ts
+++ src/app/components/user/user-bookings/user-bookings.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Inject, type OnInit } from '@angular/core'
+import { Store, select } from '@ngrx/store'
+import { getLanguage } from 'src/app/helpers/movie'
+import { type ITicketRes } from 'src/app/models/ticket'
+import { TicketService } from 'src/app/services/ticket.service'
+import { selectUserDetails } from 'src/app/states/user/user.selector'
 
 @Component({
   selector: 'app-user-bookings',
   templateUrl: './user-bookings.component.html',
   styleUrls: ['./user-bookings.component.css']
 })
-export class UserBookingsComponent {
+export class UserBookingsComponent implements OnInit {
+  userDetails$ = this.store.pipe(select(selectUserDetails))
+  userId = ''
+  tickets: ITicketRes[] = []
+  seats: string[] = []
 
+  getLanguage = getLanguage
+
+  constructor (
+    @Inject(TicketService) private readonly ticketService: TicketService,
+    @Inject(Store) private readonly store: Store
+  ) {}
+
+  ngOnInit (): void {
+    this.userDetails$.subscribe((user) => {
+      if (user !== null) this.userId = user._id
+    })
+
+    this.ticketService.getTicketsOfUser(this.userId).subscribe({
+      next: (res) => {
+        console.log(res.data, 'user tickets data')
+        this.tickets = res.data
+      }
+    })
+  }
+
+  cancelTicket (ticketId: string): void {
+
+  }
+
+  seatMapToStringArr (seatsObj: Map<string, number[]>): string[] {
+    const seats: string[] = []
+    Object.entries(seatsObj).forEach(([row, cols]: [string, number[]]) => {
+      cols.forEach(col => {
+        seats.push(row + col)
+      })
+    })
+    return seats
+  }
 }
