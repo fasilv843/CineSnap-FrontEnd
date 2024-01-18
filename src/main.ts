41faea7d3aea127e87ab85152a4ccbc730f3be2c
Fasil <fasilvaliyattil843@gmail.com>
2024-01-18 21:10:35 +0530

    env changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
