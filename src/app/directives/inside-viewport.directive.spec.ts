cbf36ebfe8c41b9d085251c64ae687e983027f4b
Fasil <fasilvaliyattil843@gmail.com>
2025-04-16 23:43:22 +0530

    deleted unnecessary files

--- src/app/directives/inside-viewport.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InsideViewportDirective } from './inside-viewport.directive';
-
-describe('InsideViewportDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InsideViewportDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/directives/seat-status.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SeatStatusDirective } from './seat-status.directive';
-
-describe('SeatStatusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SeatStatusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
